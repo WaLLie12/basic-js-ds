a4fc315189085b11bbb579a94e66c58acbf349a5
vragno01 <vadimserapionov@gmail.com>
2023-12-24 18:09:41 +0200

    feat: add binary solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,41 +6,134 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 class BinarySearchTree {
+  constructor() {
+    this.rootNode = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.rootNode = this.addRecursive(this.rootNode, data);
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addRecursive(node, data) {
+    if (node === null) {
+      return new Node(data);
+    }
+
+    if (data < node.data) {
+      node.left = this.addRecursive(node.left, data);
+    } else if (data > node.data) {
+      node.right = this.addRecursive(node.right, data);
+    }
+
+    return node;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return this.hasRecursive(this.rootNode, data);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  hasRecursive(node, data) {
+    if (node === null) {
+      return false;
+    }
+
+    if (data === node.data) {
+      return true;
+    } else if (data < node.data) {
+      return this.hasRecursive(node.left, data);
+    } else {
+      return this.hasRecursive(node.right, data);
+    }
+  }
+
+  find(data) {
+    return this.findRecursive(this.rootNode, data);
+  }
+
+  findRecursive(node, data) {
+    if (node === null) {
+      return null;
+    }
+
+    if (data === node.data) {
+      return node;
+    } else if (data < node.data) {
+      return this.findRecursive(node.left, data);
+    } else {
+      return this.findRecursive(node.right, data);
+    }
+  }
+
+  remove(data) {
+    this.rootNode = this.removeRecursive(this.rootNode, data);
+  }
+
+  removeRecursive(node, data) {
+    if (node === null) {
+      return null;
+    }
+
+    if (data === node.data) {
+      if (node.left === null) {
+        return node.right;
+      } else if (node.right === null) {
+        return node.left;
+      }
+
+      node.data = this.findMinValue(node.right);
+
+      node.right = this.removeRecursive(node.right, node.data);
+    } else if (data < node.data) {
+      node.left = this.removeRecursive(node.left, data);
+    } else {
+      node.right = this.removeRecursive(node.right, data);
+    }
+
+    return node;
+  }
+
+  findMinValue(node) {
+    while (node.left !== null) {
+      node = node.left;
+    }
+    return node.data;
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const minValueNode = this.findMinNode(this.rootNode);
+    return minValueNode ? minValueNode.data : null;
+  }
+
+  findMinNode(node) {
+    while (node.left !== null) {
+      node = node.left;
+    }
+    return node;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const maxValueNode = this.findMaxNode(this.rootNode);
+    return maxValueNode ? maxValueNode.data : null;
+  }
+
+  findMaxNode(node) {
+    while (node.right !== null) {
+      node = node.right;
+    }
+    return node;
   }
 }
 
